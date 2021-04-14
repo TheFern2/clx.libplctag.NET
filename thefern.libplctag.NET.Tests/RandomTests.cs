87aa2325d149fac5a74f731f84e2cac902b1de99
Fernando B <fernandobe+git@protonmail.com>
2021-04-14 14:03:01 -0500

    random tests

---
+++ thefern.libplctag.NET.Tests/RandomTests.cs
@@ -0,0 +1,51 @@
+﻿using libplctag.DataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thefern.libplctag.NET.Tests
+{
+    [TestClass]
+    public class RandomTests
+    {
+        const int MAX_NUMBER_TESTS = 100;
+
+        [TestMethod]
+        public async Task WriteReadBoolRandom()
+        {
+            var myPLC = new PLC(Configuration.ipAddress, Configuration.slot);
+            var rand = new Random();
+            var numberOfTests = rand.Next(0, MAX_NUMBER_TESTS);
+            var alist = new List<bool>(Randomizer.GenRandBoolList(numberOfTests));
+            Console.WriteLine("Number of random tests: " + numberOfTests);
+
+            for (int i = 0; i < alist.Count; i++)
+            {
+                await myPLC.Write("BaseBOOL", TagType.Bool, alist[i]);
+                var result = await myPLC.Read("BaseBOOL", TagType.Bool);
+                Assert.AreEqual(alist[i].ToString(), result.Value);
+                await Task.Delay(500);
+            }
+        }
+
+        [TestMethod]
+        public async Task WriteReadTagBoolRandom()
+        {
+            var myPLC = new PLC(Configuration.ipAddress, Configuration.slot);
+            var rand = new Random();
+            var numberOfTests = rand.Next(0, MAX_NUMBER_TESTS);
+            var alist = new List<bool>(Randomizer.GenRandBoolList(numberOfTests));
+            Console.WriteLine("Number of random tests: " + numberOfTests);
+
+            for (int i = 0; i < alist.Count; i++)
+            {
+                await myPLC.WriteTag<BoolPlcMapper, bool>("BaseBOOL", alist[i]);
+                var result = await myPLC.ReadTag<BoolPlcMapper, bool>("BaseBOOL");
+                Assert.AreEqual(alist[i], result.Value);
+                await Task.Delay(500);
+            }
+        }
+    }
+}
